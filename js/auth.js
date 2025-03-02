eb0008c4642f816503c50dae058c7fc7b16c4da1
unknown <1503774526@qq.com>
2025-03-02 11:08:50 +0800

    修改API

--- js/auth.js
+++ js/auth.js
@@ -2,7 +2,7 @@ const auth0 = await createAuth0Client({
     domain: 'dev-x2xv40s2d6q5f4qs.us.auth0.com',
     client_id: 'QcBaTowTveySgflw0B3i2i4aHcbGlcj3',
     redirect_uri: window.location.origin + '/callback.html',
-    audience: 'https://dev-x2xv40s2d6q5f4qs.us.auth0.com/api/v2/', // 按需配置
+    audience: 'https://sghiuang.github.io/', // 按需配置
     scope: 'openid profile email'
   });
   
